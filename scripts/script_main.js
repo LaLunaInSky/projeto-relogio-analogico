a319e35c4438d260acc394de92f5b59ef6bb65bc
LaLunaInSky <moonaflordapele@gmail.com>
2024-12-13 18:33:55 -0300

    13/12/2024 18:33

--- scripts/script_main.js
+++ scripts/script_main.js
@@ -3,9 +3,9 @@
 let ponteiroHoras = document.querySelector('.ponteiroHoras')
 let ponteiroMinutos = document.querySelector('.ponteiroMinutos')
 let ponteiroSegundos =  document.querySelector('.ponteiroSegundos')
-let segundo = 0
-let minuto = 0
-let hora = 0
+let segundo = 1000
+let minuto = 60000
+let hora = 360000
 let rotaçãoPonteiroHora = 0
 let rotaçãoPonteiroMinuto = 0
 let rotaçãoPonteiroSegundo = 0
@@ -17,8 +17,7 @@ ponteiroSegundos.style.rotate = `${rotaçãoPonteiroSegundo}deg`;
 
 
 /* 60 segundos = 1 minutos
-3.600 segundos = 1 hora
-360/60= 6deg por segundo!*/
+3.600 segundos = 1 hora*/
 
 function rodarPonteiroSegundos() {
     if (rotaçãoPonteiroSegundo == 360) {
@@ -30,4 +29,28 @@ function rodarPonteiroSegundos() {
     }
 }
 
-setInterval(rodarPonteiroSegundos, 166)
+function rodarPonteiroMinutos() {
+    if (rotaçãoPonteiroMinuto == 360) {
+        rotaçãoPonteiroMinuto = 0
+        ponteiroMinutos.style.rotate = `${rotaçãoPonteiroMinuto}deg`;
+    } else {
+        rotaçãoPonteiroMinuto += 0.5 // 1 minuto = 0.5deg
+        minuto ++
+        ponteiroMinutos.style.rotate = `${rotaçãoPonteiroMinuto}deg`;
+    }
+}
+
+function rodarPonteiroHoras() {
+    if (rotaçãoPonteiroHora == 360) {
+        rotaçãoPonteiroHora = 0
+        ponteiroHoras.style.rotate = `${rotaçãoPonteiroHora}deg`;
+    } else {
+        rotaçãoPonteiroHora += 30 // 1 hora = 30deg 
+        ponteiroHoras.style.rotate = `${rotaçãoPonteiroHora}deg`;
+    }
+}
+
+
+setInterval(rodarPonteiroSegundos, 270)
+setInterval(rodarPonteiroMinutos, 60000)
+setInterval(rodarPonteiroHoras, 3600000)
