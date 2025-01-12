202faa95a0eacb8b352056dfa695d60cb8cea0a0
LaLunaInSky <moonaflordapele@gmail.com>
2025-01-12 11:08:35 -0300

    12/01/2025 11:08

--- scripts/script_main.js
+++ scripts/script_main.js
@@ -2,14 +2,22 @@
 
 const mostradorRelógio = document.querySelector('.mostradorRelógio')
 
-for (let countHoras = 1; countHoras < 7; countHoras++) {
+for (let countHoras = 1; countHoras < 13; countHoras++) {
     const traçoHorasGerenciador = document.createElement("section")
-    traçoHorasGerenciador.setAttribute('class', 'traço')
-    traçoHorasGerenciador.setAttribute('id', `h${countHoras}`)
+    traçoHorasGerenciador.setAttribute('class', 'traço')    
+    if (countHoras < 7) {
+        traçoHorasGerenciador.setAttribute('id', `h${countHoras}`)
+    } else {
+        traçoHorasGerenciador.setAttribute('id', `meiaH${countHoras - 6}`)
+    }
 
     for (let countHoraTraço = 1; countHoraTraço < 3; countHoraTraço++) {
         const traçoHora = document.createElement('div')
-        traçoHora.setAttribute('class', 'traçoHora')
+        if (countHoras < 7) {
+            traçoHora.setAttribute('class', 'traçoHora')
+        } else {
+            traçoHora.setAttribute('class', 'traçoMeiaHora')
+        }
         traçoHorasGerenciador.appendChild(traçoHora)
     }
 
